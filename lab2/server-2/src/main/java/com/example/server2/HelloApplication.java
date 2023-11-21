138cd3fbd7702502316f6be5a30802859f2a2bc6
Aleksandr <snt17gm@gmail.com>
2023-11-22 00:49:53 +0300

    fix

--- lab2/server-2/src/main/java/com/example/server2/HelloApplication.java
+++ lab2/server-2/src/main/java/com/example/server2/HelloApplication.java
@@ -1,9 +1,19 @@
 package com.example.server2;
 
+import com.example.server2.configuration.HttpsFilter;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/")
 public class HelloApplication extends Application {
-
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(HttpsFilter.class);
+        classes.add(HrResource.class);
+        return classes;
+    }
 }
