64cc1f1f142b814145f92466349777044d5a0f7c
Aleksandr <snt17gm@gmail.com>
2023-11-21 16:03:27 +0300

    456

--- lab2/soa-lab2-server1 - without ssl/src/main/java/com/example/soalab2server1/dao/model/Position.java
+++
@@ -1,9 +0,0 @@
-package com.example.soalab2server1.dao.model;
-
-public enum Position {
-            MANAGER,
-            HUMAN_RESOURCES,
-            HEAD_OF_DEPARTMENT,
-            DEVELOPER,
-            COOK
-}
