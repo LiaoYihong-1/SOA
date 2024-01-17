cb1c98a90cb78e08c7af02319c10ddc1c5684479
Aleksandr <snt17gm@gmail.com>
2024-01-17 19:53:59 +0300

    fix

--- lab2/service1/service-1/src/main/java/com/example/soalab2server1/SoaLab2Server1Application.java
+++ lab2/service1/service-1/src/main/java/com/example/soalab2server1/SoaLab2Server1Application.java
@@ -2,12 +2,12 @@ package com.example.soalab2server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableJpaAuditing
-@SpringBootApplication
+@EnableWebMvc
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class SoaLab2Server1Application {
 
